1f64995d46136e1eacd079f7ee82c1b2f6db0038
bensmithperez <bsmith@scnctech.com>
2023-12-21 17:27:49 -0800

    agrego dir y mejoro cosas

--- loadData.js
+++ loadData.js
@@ -5,14 +5,14 @@ function getParameterByName(name) {
     return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 };
 
-function armarLinkExample(dato){
+function armarLinkExample(dato) {
     // necesito algo así https://www.youtube.com/embed/hUjmO1_6QUQ?si=wctEl3ra5JQTBpJ2&amp;clip=Ugkxugi9ZCHZURLU5dE1IyWBqqAmX7zsvERE&amp;clipt=EPCKDBj4sQw
 
     // recibo algo así hUjmO1_6QUQ?wctEl3ra5JQTBpJ2?Ugkxugi9ZCHZURLU5dE1IyWBqqAmX7zsvERE?EPCKDBj4sQw
     var link = dato.split("?");
     linkExample = null;
     // armo el link
-    if (link.length == 3){
+    if (link.length == 3) {
         // https://www.youtube.com/embed/Naxqk6buRZY?clip=UgkxiBURTyALMmkF4MeKb3zVmsRH937niO0j&clipt=ELWouAEYvc-4AQ
         var linkExample = "https://www.youtube.com/embed/" + link[0] + "?clip=" + link[1] + "&clipt=" + link[2];
     }
@@ -23,28 +23,36 @@ if (getParameterByName("streamer-name") != null) {
     h1Name = document.getElementById("h1_name");
     pPronunciation = document.getElementById("p_pronunciation");
     console.log(getParameterByName("streamer-name"));
-    
+
     if (pronunciation[getParameterByName("streamer-name")] != null) {
         h1Name.innerHTML = getParameterByName("streamer-name");
         for (var i = 0; i < pronunciation[getParameterByName("streamer-name")].length; i++) {
             pPronunciation.innerHTML += pronunciation[getParameterByName("streamer-name")][i] + "<br>";
         }
-        
+
         if (examples[getParameterByName("streamer-name")] != null) {
             var examplesDiv = document.getElementById("div_examples");
-            for (var i = 0; i < examples[getParameterByName("streamer-name")].length; i++) {
-                var iframe = document.createElement("iframe");
-                var linkFinal = armarLinkExample(examples[getParameterByName("streamer-name")][i]);
-                // var linkFinal = examples[getParameterByName("streamer-name")][i];
-                if (linkFinal != null){
-                    iframe.src = linkFinal;
-                    iframe.width = "200";
-                    iframe.height = "100";
-                    iframe.frameborder = "0";
-                    iframe.allow = "encrypted-media";
-                    iframe.allowfullscreen = "";
-                    examplesDiv.appendChild(iframe);
+
+            if (examples[getParameterByName("streamer-name")].length > 0) {
+                for (var i = 0; i < examples[getParameterByName("streamer-name")].length; i++) {
+                    var iframe = document.createElement("iframe");
+                    var linkFinal = armarLinkExample(examples[getParameterByName("streamer-name")][i]);
+                    // var linkFinal = examples[getParameterByName("streamer-name")][i];
+                    if (linkFinal != null) {
+                        iframe.src = linkFinal;
+                        iframe.width = "200";
+                        iframe.height = "100";
+                        iframe.frameborder = "0";
+                        iframe.allow = "encrypted-media";
+                        iframe.allowfullscreen = "";
+                        examplesDiv.appendChild(iframe);
+                    }
                 }
+            } else {
+                var examplesDiv = document.getElementById("div_examples");
+                var p = document.createElement("p");
+                p.innerHTML = "No examples found.";
+                examplesDiv.appendChild(p);
             }
         } else {
             var examplesDiv = document.getElementById("div_examples");
---
+++ loadDirectory.js
@@ -0,0 +1,47 @@
+function verLetraInicial(datoActual, datoAnterior) {
+    if (datoActual.charAt(0) != datoAnterior.charAt(0)) {
+        return datoActual.charAt(0);
+    } else {
+        return false;
+    }
+}
+
+function loadDirectory() {
+    console.log("loadDirectory");
+    console.log(pronunciation);
+    var names = document.getElementById("directory_names");
+    var nombreAnterior = "";
+    for (var streamer in pronunciation) {
+        // genero nuevo streamer
+        var divPrincipal = document.createElement("div");
+        divPrincipal.setAttribute("class", "directory_name--streamer");
+        //nombre
+        var pNombre = document.createElement("p");
+        pNombre.setAttribute("class", "streamer_nombre");
+        pNombre.innerHTML = streamer;
+        // pronunciacion
+        var pPronunciacion = document.createElement("p");
+        pPronunciacion.setAttribute("class", "streamer_pronunciacion");
+        pPronunciacion.innerHTML = pronunciation[streamer];
+        // link
+        var link = document.createElement("a");
+        link.setAttribute("class", "streamer_link");
+        link.setAttribute("href", "name.html?streamer-name=" + streamer);
+        link.innerHTML = "View more";
+
+        divPrincipal.appendChild(pNombre);
+        divPrincipal.appendChild(pPronunciacion);
+        divPrincipal.appendChild(link);
+        
+        // para la letra grande
+        var letraInicial = verLetraInicial(streamer, nombreAnterior);
+        if (letraInicial) {
+            var divLetra = document.createElement("div");
+            divLetra.setAttribute("class", "directory_name--letra");
+            divLetra.innerHTML = letraInicial;
+            names.appendChild(divLetra);
+        }
+        names.appendChild(divPrincipal);
+        nombreAnterior = streamer;
+    }
+}
